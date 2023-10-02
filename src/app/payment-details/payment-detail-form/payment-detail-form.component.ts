04465dbe698d28d2d20ca245fe1c612a6f668301
Makhmud <makhmudkuzikaev@gmail.com>
2023-10-02 12:17:03 +0600

    finish project

--- src/app/payment-details/payment-detail-form/payment-detail-form.component.ts
+++ src/app/payment-details/payment-detail-form/payment-detail-form.component.ts
@@ -16,8 +16,19 @@ export class PaymentDetailFormComponent {
   }
 
   onSubmit(form: NgForm){
+    this.service.formSubmitted = true;
     if(form.valid){
-      this.service.postPaymentDetail()
+      if(this.service.formData.paymentDetailId == 0){
+        this.insertRecord(form)
+      }else{
+        this.updateRecord(form)
+      }
+    }
+    
+  }
+
+  insertRecord(form: NgForm){
+    this.service.postPaymentDetail()
       .subscribe({
         next: res => {
           this.service.list = res as PaymentDetail[]
@@ -26,7 +37,17 @@ export class PaymentDetailFormComponent {
         },
         error: err => { console.log(err) }
       })
-    }
-    
+  }
+
+  updateRecord(form: NgForm){
+    this.service.putPaymentDetail()
+    .subscribe({
+      next: res => {
+        this.service.list = res as PaymentDetail[]
+        this.service.reserForm(form)
+        this.toastr.info('Updated successfully', 'Payment Detail Register')
+      },
+      error: err => { console.log(err) }
+    })
   }
 }
--- src/app/shared/payment-detail.service.ts
+++ src/app/shared/payment-detail.service.ts
@@ -12,6 +12,7 @@ export class PaymentDetailService {
   url: string = environment.apiBaseUrl + '/PaymentDetails'
   list: PaymentDetail[] = [];
   formData: PaymentDetail = new PaymentDetail()
+  formSubmitted: boolean = false;
   constructor(private http: HttpClient) { }
 
   refreshList() {
@@ -28,8 +29,16 @@ export class PaymentDetailService {
     return this.http.post(this.url, this.formData)
   }
 
+  putPaymentDetail() {
+    return this.http.put(this.url +'/' + this.formData.paymentDetailId, this.formData)
+  }
+  
+  deletePaymentDetail(id: number) {
+    return this.http.delete(this.url +'/' + id)
+  }
   reserForm(form: NgForm){
     form.form.reset()
     this.formData = new PaymentDetail()
+    this.formSubmitted = false;
   }
 }
